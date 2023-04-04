32c13e8f14c9f01845ebccd318c4b107d980beb7
Rupert <r-denner@gmx.de>
2023-04-04 06:41:43 +0200

    comminication changed

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-import time
+from time import time, sleep
 from rupcom import Serial_com
 from stepmotor import motor
 from encoder import encoder
 import threading
 
 if __name__ == "__main__":
+    starttime=time()
     ser=Serial_com()
-    ser.open("/dev/ttyAMA0",9600)
+    ser.open("/dev/ttyAMA0",56000)
     zmotor=motor(12, 16, 18, 22)
     zmotor.motorinit()
     zencoder=encoder(32)
     zencoder.ecoderInit()
+    motorpositionalt=0
     #zmotor.move(20480,'cw')
     #time.sleep(4)
     #zmotor.motorStop()
     try:
        while True:
-           ser.write(str(zmotor.steps)+'\n')
+           #send='pos'+str(zmotor.position)+'\n'
+           t=int(time()-starttime)/60#+(time()-starttime)%60*0.4
+           print(t)
+           if motorpositionalt != zmotor.position:
+              print(t)   
+              ser.write('pos'+str(zmotor.position)+'\n')
+              ser.write('enc'+str(zencoder.getspeed())+'\n')
+           #print(send)
            message=str(ser.read())
-           #print(message)
+           print(message)
            if message[2]=='a':    
                 zmotor.stop=False
                 zmotor.move(2048,'cw')
@@ -29,24 +37,25 @@ if __name__ == "__main__":
                 zmotor.stop=False
                 zmotor.move(2048,'ccw')
            if message[2]=='c':
-               steps=message[3]+message[4]+message[5]+message[6]+message[7]
+               steps=message[3:7]
                print(steps)
                if steps != '     ':
                     zmotor.stop=False
                     zmotor.move(int(steps),'cw')
            if message[2]=='d':
-               steps=message[3]+message[4]+message[5]+message[6]+message[7]
+               steps=message[3:7]
                print(steps)
                if steps != '     ':
                     zmotor.stop=False
                     zmotor.move(int(steps),'ccw')
-                    ser.write(str(zmotor.steps)+'\n')
+                    #ser.write(str(zmotor.steps)+'\n')
            print(zencoder.getspeed())
-           #print(zmotor.position)
+           print(zmotor.position)
+           print(zmotor.steps)
            #if zmotor.position >= 400:
                #zmotor.motorStop()
-            
-           time.sleep(1)
+           motorpositionalt=zmotor.position 
+           sleep(0.2)
            
     except KeyboardInterrupt:
         zmotor.motordeinit()
